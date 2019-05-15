9e87cdce64446345bc6bb87d5b8c5d8d18ce9927
Tamago-san <keitaidenwa1995@gmail.com>
2019-05-15 17:40:18 +0900

    20190515

--- sin_predict.py
+++ sin_predict.py
@@ -11,21 +11,29 @@ from keras.layers import GRU
 import matplotlib.pyplot as plt
 
 
+
+GOD_step=50
+in_out_neurons = 1
+hidden_neurons = 10
+length_of_sequences = 100
+epoch=100
+
+
 def _load_data(data, n_prev = 100):
     """
     data should be pd.DataFrame()
     """
 
     docX, docY = [], []
-    for i in range(len(data)-n_prev):
+    for i in range(len(data)-n_prev-GOD_step+1):
         docX.append(data.iloc[i:i+n_prev].as_matrix())
-        docY.append(data.iloc[i+n_prev].as_matrix())
+        docY.append(data.iloc[i+n_prev+GOD_step-1].as_matrix())
     alsX = np.array(docX)
     alsY = np.array(docY)
 
     return alsX, alsY
 
-def train_test_split(df, test_size=0.1, n_prev = 100):
+def train_test_split(df, test_size=0.2, n_prev = 100):
     """
     This just splits data to training and testing parts
     """
@@ -34,35 +42,38 @@ def train_test_split(df, test_size=0.1, n_prev = 100):
     X_train, y_train = _load_data(df.iloc[0:ntrn], n_prev)
     X_test, y_test = _load_data(df.iloc[ntrn:], n_prev)
 
+
     return (X_train, y_train), (X_test, y_test)
 
-random.seed(0)
-# 乱数の係数
-random_factor = 0.05
-# サイクルあたりのステップ数
-steps_per_cycle = 80
-# 生成するサイクル数
-number_of_cycles = 50
 
-df = pd.DataFrame(np.arange(steps_per_cycle * number_of_cycles + 1), columns=["t"])
-df["sin_t"] = df.t.apply(lambda x: math.sin(x * (2 * math.pi / steps_per_cycle)+ random.uniform(-1.0, +1.0) * random_factor))
-df[["sin_t"]].head(steps_per_cycle * 2).plot()
-    
-length_of_sequences = 100
-(X_train, y_train), (X_test, y_test) = train_test_split(df[["sin_t"]], n_prev =length_of_sequences)
 
-in_out_neurons = 1
-hidden_neurons = 10
+df = pd.read_csv('./data/output_Runge_Lorenz.csv',
+#                    usecols=[1],
+                    engine='python',
+                    header =None
+                )
+
+df = df.rename(columns={0: 'X'})
+df = df.rename(columns={1: 'Y'})
+df = df.rename(columns={2: 'Z'})
+#df[["sin_t"]].head(steps_per_cycle * 2).plot()
+
+
+(X_train, y_train), (X_test, y_test) = train_test_split(df[["X"]], n_prev =length_of_sequences)
+
 
 model = Sequential()
 model.add(LSTM(hidden_neurons, batch_input_shape=(None, length_of_sequences, in_out_neurons), return_sequences=False))
 model.add(Dense(in_out_neurons))
 model.add(Activation("linear"))
 model.compile(loss="mean_squared_error", optimizer="rmsprop")
-model.fit(X_train, y_train, batch_size=600, nb_epoch=15, validation_split=0.05)
+model.fit(X_train, y_train, batch_size=600, nb_epoch=epoch, validation_split=0.05)
 
 predicted = model.predict(X_test)
 dataf =  pd.DataFrame(predicted[:200])
 dataf.columns = ["predict"]
 dataf["input"] = y_test[:200]
 print(dataf)
+#plt.figure()
+dataf.plot()
+plt.show()
